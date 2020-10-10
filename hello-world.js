07b44d81f5b547a48db2fbbb93aa1e9dc1cef554
jojomango <q9898989@gmail.com>
2020-10-10 22:22:44 +0800

    add .env

--- hello-world.js
+++ hello-world.js
@@ -8,4 +8,15 @@ app.get('/', (req, res) => {
   res.send('I bought this domain! ~jojomango99');
 });
 
+app.get('/times', (req, res) => res.send(showTimes()))
+
+const showTimes() {
+  let result = '';
+  const times = process.env.TIMES || 5;
+  for (i = 0; i < times; i++) {
+    result += i + ' ';
+  }
+  return result;
+}
+
 app.listen(PORT, () => console.log(`Hello world app listening on port ${PORT}!`))
