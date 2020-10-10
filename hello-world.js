e62d670f5a178297db0476b391d2ca6ea9377675
jojomango <q9898989@gmail.com>
2020-10-10 21:38:14 +0800

    port typo

--- hello-world.js
+++ hello-world.js
@@ -8,4 +8,4 @@ app.get('/', (req, res) => {
   res.send('Hello World, from express');
 });
 
-app.listen(port, () => console.log(`Hello world app listening on port ${port}!`))
+app.listen(PORT, () => console.log(`Hello world app listening on port ${PORT}!`))
